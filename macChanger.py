a04990e38ebf1a5902b1018338e69f50b5ea8cf7
ankit-cn-47 <ankit.codenamefourty7@gmail.com>
2020-06-16 00:38:03 -0400

    mac changer complete

--- macChanger.py
+++ macChanger.py
@@ -1,3 +1,17 @@
 import subprocess
-subprocess.call('ifconfig', shell=True)
+
+interface = input("interface >")
+
+new_mac = input('new MAC (00:XX:XX:XX:XX:XX)>')
+
+print('[+] Changing mac address for ' + interface + ' to ' + new_mac)
+
+print("ifconfig " + interface + " down")
+subprocess.call("ifconfig " + interface + " down", shell=True)
+
+print("ifconfig " + interface + " hw ether " + new_mac)
+subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+
+print("ifconfig " + interface + " up")
+subprocess.call("ifconfig " + interface + " up", shell=True)
 
